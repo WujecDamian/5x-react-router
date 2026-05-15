d2d606953c8112b9de2e554f0da3a57b5ec4a60a
Damian Wujec <wujecdamian12@gmail.com>
2026-05-15 11:13:36 +0200

    feat: replace anchor tag with Link component for navigation to Profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import "./App.css";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <nav>
         <ul>
           <li>
-            <a href="profile">Profile page</a>
+            <Link to="profile">Profile page</Link>
           </li>
         </ul>
       </nav>
